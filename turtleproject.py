0f38969e5b22efcde5fc7a918d5bca39060ed145
p22dhealy <p22dhealy@fairfieldprep.org>
2022-03-30 10:11:43 -0400

    Turtle project day 2

--- turtleproject.py
+++ turtleproject.py
@@ -2,50 +2,72 @@
 
 import turtle
 
-window = turtle.Screen() # window for the turtle
+
+#setting up window
+window = turtle.Screen() 
 window.setup(1000, 1000)
 window.screensize(100,100)
-
 Declan = turtle.Turtle()
 
 
 
 
-
-def move_forward():
-    if Declan.direction != "Up":
-        Declan
-    elif Declan.direction == "Down":
-        Declan.direction == "Up"
-        Declan.forward(50)
-
-
-
-
-window.onkey(move_forward, "Up")
+#moving functions 
+def move_up():
+    Declan.setheading(90)
+    Declan.forward(50)
 
 def move_right():
-    Declan.right(90)
+    Declan.setheading(0)
     Declan.forward(50)
-
-window.onkey(move_right, "Right")
 
 def move_left():
-    Declan.left(90)
+    Declan.setheading(180)
     Declan.forward(50)
-
-window.onkey(move_left, "Left")
 
 def move_down():
-    Declan.right(180)
+    Declan.setheading(270)
     Declan.forward(50)
+    
 
+#moving key binds 
+window.onkey(move_left,"Left")
 
+window.onkey(move_right,"Right")
 
+window.onkey(move_up,"Up")
 
-
+window.onkey(move_down,"Down")
 
 window.listen()
+
+# Draw border
+mypen = turtle.Turtle()
+mypen.penup()
+mypen.setposition(-300,-300)
+mypen.pendown()
+mypen.pensize(3)
+for side in range(4):
+    mypen.forward(600)
+    mypen.left(90)
+mypen.hideturtle()
+
+
+
+
+#making boundary untouchable    
+while True:
+    #boundary x cord
+    if Declan.xcor() > 290 or Declan.xcor() < -290:
+        Declan.right(180)
+
+    #boundary y cord 
+    if Declan.ycor() > 290 or Declan.ycor() < -290:
+        Declan.right(180)
+
+
+
+
 
 
 window.mainloop()
